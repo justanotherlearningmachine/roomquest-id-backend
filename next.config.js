5d21775e1678229247592fa32b7eb0c82ca4251d
justanotherlearningmachine <justanotherlearningmachine@gmail.com>
2025-11-19 21:35:25 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,10 @@ module.exports = {
       {
         source: '/api/:path*',
         headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
         ],
       },
     ];
